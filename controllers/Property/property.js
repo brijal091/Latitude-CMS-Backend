0a3be5768b3a3d4d7f08541d2c8707e78d12b99b
Brijal Kansara <brijal.k@latitudetechnolabs.org>
2024-01-12 11:38:29 +0530

    edit property API

--- controllers/Property/property.js
+++ controllers/Property/property.js
@@ -35,11 +35,9 @@ const createProperty = async (req, res) => {
       configurations,
     });
     if (!property)
-      return res
-        .status(400)
-        .json({
-          message: "Property not created, please try again sometimes later",
-        });
+      return res.status(400).json({
+        message: "Property not created, please try again sometimes later",
+      });
     return res
       .status(200)
       .json({ message: "Your Property has been registered. Thank you." });
@@ -49,22 +47,102 @@ const createProperty = async (req, res) => {
 };
 // get Property API controller
 const getProperty = async (req, res) => {
-  const subdomain = req.headers.host;
-  const property = await Property.findById(req.params.id);
-  if (!property || property.subDomain !== subdomain)
-    return res.status(404).send("No such property found");
-  return res.status(200).json(property);
+  try {
+    const subdomain = req.headers.host;
+    const property = await Property.findById(req.params.id);
+    if (!property || property.subDomain !== subdomain)
+      return res.status(404).send("No such property found");
+    return res.status(200).json(property);
+  } catch (error) {
+    console.log(error);
+    return res.send(500).json({ error: "Something went wrong" });
+  }
 };
 
 // get all Properties API controller
 const getAllProperties = async (req, res) => {
-  const subDomain = req.headers.host;
-  const properties = await Property.find({subDomain});
-  return res.status(200).json(properties);
+  try {
+    const subDomain = req.headers.host;
+    const properties = await Property.find({ subDomain });
+    return res.status(200).json(properties);
+  } catch (error) {
+    console.log(error);
+    return res.send(500).json({ error: "Something went wrong" });
+  }
+};
+
+// get all Properties API controller
+const deleteProperty = async (req, res) => {
+  try {
+    const subDomain = req.headers.host;
+    const id = req.params.id;
+    const properties = await Property.findOneAndDelete({ _id: id, subDomain });
+    if (!properties) return res.status(404).send("No Such Property Found.");
+    else {
+      return res.status(200).send("Successfully Deleted the Property.");
+    }
+  } catch (error) {
+    console.log(error);
+    return res.send(500).json({ error: "Something went wrong" });
+  }
+};
+
+// edit property
+const editProperty = async (req, res) => {
+  const {id} = req.params;
+  const subdomain = req.headers.host
+  const {
+    propertyTitle,
+    address,
+    location,
+    amenities,
+    subDomain,
+    desc,
+    email,
+    phone,
+    agentName,
+    size,
+    type,
+    price,
+    configurations,
+  } = req.body;
+  try {
+    // Assuming you have a Mongoose Property model
+  const updatedProperty = await Property.findOneAndUpdate(
+    { _id: id },
+    {
+      $set: {
+        propertyTitle,
+        address,
+        location,
+        amenities,
+        desc,
+        email,
+        phone,
+        agentName,
+        size,
+        type,
+        price,
+        configurations,
+      },
+    },
+    { new: true } // This option returns the updated document
+  );
+
+  if (!updatedProperty) {
+    return res.status(404).json({ error: 'Property not found' });
+  }
+
+  return res.status(200).json(updatedProperty);
+  } catch (error) {
+    console.log("Internal server error", error);
+  }
 };
 
 module.exports = {
   createProperty,
   getProperty,
   getAllProperties,
+  deleteProperty,
+  editProperty,
 };
--- routes/Property/property.js
+++ routes/Property/property.js
@@ -2,19 +2,35 @@ const express = require("express");
 const router = express.Router();
 
 // Require controller modules.
-let {createProperty, getProperty, getAllProperties} = require("../../controllers/Property/property");
+let {
+  createProperty,
+  getProperty,
+  getAllProperties,
+  deleteProperty,
+  editProperty
+} = require("../../controllers/Property/property");
 
 // Create Property API
 router.post("/add-property", (req, res) => {
-    createProperty(req, res);
-    });
+  createProperty(req, res);
+});
 // Get Property Details based on id
 router.get("/get-property/:id", (req, res) => {
-    getProperty(req,res)
-}) 
+  getProperty(req, res);
+});
 
 // Get Properties
 router.get("/getallproperties", (req, res) => {
-    getAllProperties(req,res)
-}) 
+  getAllProperties(req, res);
+});
+
+// Edit Property
+router.put("/edit-property/:id", (req, res) => {
+  editProperty(req, res);
+});
+
+// Delete Property
+router.delete("/delete-property/:id", (req, res) => {
+  deleteProperty(req, res);
+});
 module.exports = router;
